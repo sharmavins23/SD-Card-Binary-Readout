9d84ede8b175808516c09ff6408f7e5ca3e46611
Vineeth Sharma <sharmavins23@gmail.com>
2020-12-24 21:17:29 -0500

    added top level design file

---
+++ modules/SDCardBinaryReadout.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/24/2020 09:17:05 PM
+// Design Name: 
+// Module Name: SDCardBinaryReadout
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SDCardBinaryReadout(
+
+    );
+endmodule
