b85f9b414e8b0bf7dc97893a618c1b5b2d268253
Vineeth Sharma <sharmavins23@gmail.com>
2020-12-24 21:29:57 -0500

    added 4in mux

--- modules/Mux4Bit4Input.v
+++ modules/Mux4Bit4Input.v
@@ -1,26 +1,28 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/24/2020 07:46:35 PM
-// Design Name: 
-// Module Name: Mux4Bit4Input
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Module Name: Multiplexer - 4 Bit, 4 Input
+//
+// Description: Selects between four individual input signals.
+////////////////////////////////////////////////////////////////////////////////
 
 
 module Mux4Bit4Input(
-
+    // Inputs
+    input [1:0] selector,
+    input [3:0] nibbleA,
+    input [3:0] nibbleB,
+    input [3:0] nibbleC,
+    input [3:0] nibbleD,
+    // Outputs
+    output reg [3:0] nibbleOut
     );
+
+    always @(*) begin
+        case(selector)
+            2'b11: nibbleOut = nibbleA;
+            2'b10: nibbleOut = nibbleB;
+            2'b01: nibbleOut = nibbleC;
+            2'b00: nibbleOut = nibbleD;
+        endcase
+    end
 endmodule
