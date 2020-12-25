fff8a12d7a24ba1edd0485a2a9a5132925bc7ff8
Vineeth Sharma <sharmavins23@gmail.com>
2020-12-24 19:47:31 -0500

    added src files for 7seg out

---
+++ modules/Mux4Bit4Input.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/24/2020 07:46:35 PM
+// Design Name: 
+// Module Name: Mux4Bit4Input
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Mux4Bit4Input(
+
+    );
+endmodule
