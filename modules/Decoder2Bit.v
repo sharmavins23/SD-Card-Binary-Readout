0e2205e1176c8f950f1c6781737d26816d95e881
Vineeth Sharma <sharmavins23@gmail.com>
2020-12-24 21:36:51 -0500

    added decoder definition

--- modules/Decoder2Bit.v
+++ modules/Decoder2Bit.v
@@ -1,26 +1,25 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/24/2020 07:46:35 PM
-// Design Name: 
-// Module Name: Decoder2Bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Module Name: Decoder - 2 Bit
+//
+// Description: Takes a two bit input signal and decodes it to four outputs.
+//              Outputs are serialized in a bus.
+////////////////////////////////////////////////////////////////////////////////
 
 
-module Decoder2Bit(
-
+module Decoder2Bit (
+    // Inputs
+    input [1:0] signalIn,
+    // Outputs
+    output reg [3:0] signalOut
     );
+
+    always @(*) begin
+        case(signalIn):
+            2'b11: signalOut = 4'b1000;
+            2'b10: signalOut = 4'b0100;
+            2'b01: signalOut = 4'b0010;
+            2'b00: signalOut = 4'b0001;
+        endcase
+    end
 endmodule
